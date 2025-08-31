9d5ce457fbea8981ff5a7eb38bedd351cc6b6dbe
Dristi Roy <dristiroy@gmail.com>
2025-08-31 07:44:41 -0700

    inv progress
    borrowing baseline, now writing to fit my game

--- inventory/inv_ui_slot.gd
+++ inventory/inv_ui_slot.gd
@@ -2,14 +2,14 @@ extends Panel
 
 @onready var item_visual: Sprite2D = $CenterContainer/Panel/item_display
 
-func update(item: InvItem):
-	if !item:
+func update(slot: InvSlot):
+	if !slot:
 		item_visual.visible = false
 	else:
 		item_visual.visible = true
-		item_visual.texture = item.texture
-		if item.texture:
-			var tex_size = item.texture.get_size()
+		item_visual.texture = slot.texture
+		if slot.texture:
+			var tex_size = slot.texture.get_size()
 			var target_size = Vector2(16, 16)
 			var scale_factor = min(target_size.x / tex_size.x, target_size.y / tex_size.y)
 			item_visual.scale = Vector2.ONE * scale_factor
--- inventory/inventory.gd
+++ inventory/inventory.gd
@@ -1,5 +1,16 @@
 extends Resource
-
 class_name Inv
 
-@export var items: Array[InvItem]
+@export var slots: Array[InvSlot]
+
+func insert(item: InvItem, amount: int = 1) -> bool:
+	for slot in slots:
+		if slot.item == item:
+			slot.amount += amount
+			return true
+	for slot in slots:
+		if slot.item == null:
+			slot.item = item
+			slot.amount = amount
+			return true
+	return false
